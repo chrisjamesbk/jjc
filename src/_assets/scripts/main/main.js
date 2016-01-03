94bafffa1cdf8fc10d2f2cb585eafa94bd2001db
Chris James <chris@work.co>
2016-01-03 18:57:20 -0500

    Scroll behavior

--- src/_assets/scripts/main/main.js
+++ src/_assets/scripts/main/main.js
@@ -1,3 +1,24 @@
+var $window = $(window);
+
+var BREAKPOINT = {
+  '--sm-viewport': '(min-width:0)',
+  '--md-viewport': '(min-width:780px)',
+  '--lg-viewport': '(min-width:1040px)',
+};
+
+var getBreakpoint = function() {
+  if (window.matchMedia(BREAKPOINT['--lg-viewport']).matches) {
+    return 'large';
+  } else if (window.matchMedia(BREAKPOINT['--md-viewport']).matches) {
+    return 'medium';
+  } else if (window.matchMedia(BREAKPOINT['--sm-viewport']).matches) {
+    return 'small';
+  }
+};
+
+
+// Click handlers
+
 $('#js-toggle-nav').on('click', function() {
   $(this).toggleClass('isNavOpen');
   $('.Navigation').toggleClass('isOpen');
@@ -15,4 +36,23 @@ $('.Navigation-item').on('click', function(e) {
   var offset = $('#' + target).offset().top;
 
   $('html, body').animate({ scrollTop: offset - 60 }, 300);
+});
+
+
+// Scroll handler
+
+var offset = getBreakpoint() === 'large' ? 364 : 302;
+var isSticky = $window.scrollTop() >= offset;
+
+if (isSticky) {
+  $('body').addClass('is-headerSticky');
+}
+
+$window.on('scroll', function() {
+  var scrollTop = $window.scrollTop();
+  var offset = getBreakpoint() === 'large' ? 364 : 302;
+  var isSticky = scrollTop >= offset;
+  var action = isSticky ? 'addClass' : 'removeClass';
+
+  $('body')[action]('is-headerSticky');
 });
