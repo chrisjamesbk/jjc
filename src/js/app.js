3f917762f2f4cb4afd2cf63e4a75542ccce918ff
Chris James <chris@work.co>
2021-01-25 12:22:23 -0500

    v2.0 beta

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-console.log('test');
-
 var BREAKPOINT = {
   '--sm-viewport': '(min-width:0)',
   '--md-viewport': '(min-width:780px)',
@@ -88,40 +86,40 @@ var scrollTo = function(scrollTo, scrollDuration) {
 	window.requestAnimationFrame(step);
 };
 
-var navToggle = document.getElementById('js-toggle-nav');
-var nav = document.getElementsByClassName('Navigation')[0];
-var navItems = document.getElementsByClassName('Navigation-item');
+// var navToggle = document.getElementById('js-toggle-nav');
+// var nav = document.getElementsByClassName('Navigation')[0];
+// var navItems = document.getElementsByClassName('Navigation-item');
 var email = document.getElementsByClassName('Footer--email')[0];
 var phone = document.getElementsByClassName('Footer--phone')[0];
 
-var onNavClick = function() {
-  navToggle.classList.toggle('isNavOpen');
-  nav.classList.toggle('isOpen');
-  document.body.classList.toggle('isNavOpen');
-};
+// var onNavClick = function() {
+//   navToggle.classList.toggle('isNavOpen');
+//   nav.classList.toggle('isOpen');
+//   document.body.classList.toggle('isNavOpen');
+// };
 
-var onNavItemClick = function(e) {
-  e.preventDefault();
-  console.log('click');
+// var onNavItemClick = function(e) {
+//   e.preventDefault();
+//   console.log('click');
 
-  navToggle.classList.remove('isNavOpen');
-  nav.classList.remove('isOpen');
-  document.body.classList.remove('isNavOpen');
+//   navToggle.classList.remove('isNavOpen');
+//   nav.classList.remove('isOpen');
+//   document.body.classList.remove('isNavOpen');
 
-  var target = this.getAttribute('data-anchor');
-  var offset = document.getElementById(target).offsetTop;
+//   var target = this.getAttribute('data-anchor');
+//   var offset = document.getElementById(target).offsetTop;
 
-  ga('send', 'event', 'Navigation', 'click', target);
+//   ga('send', 'event', 'Navigation', 'click', target);
 
-  scrollTo(offset - 60, 300);
-};
+//   scrollTo(offset - 60, 300);
+// };
 
 // Click handlers
 
-navToggle.addEventListener('click', onNavClick);
-for (var i = 0; i < navItems.length; i++) {
-  navItems[i].addEventListener('click', onNavItemClick);
-}
+// navToggle.addEventListener('click', onNavClick);
+// for (var i = 0; i < navItems.length; i++) {
+//   navItems[i].addEventListener('click', onNavItemClick);
+// }
 email.addEventListener('click', function() {
   ga('send', 'event', 'Contact', 'click', 'email');
 });
@@ -131,19 +129,33 @@ phone.addEventListener('click', function() {
 
 // Scroll handler
 
-var offset = getBreakpoint() === 'large' ? 364 : 302;
-var isSticky = window.scrollY >= offset;
+// var offset = getBreakpoint() === 'large' ? 364 : 302;
+// var isSticky = window.scrollY >= offset;
 
-if (isSticky) {
-  document.body.classList.add('is-headerSticky');
+// if (isSticky) {
+//   document.body.classList.add('is-headerSticky');
+// }
+
+// window.addEventListener('scroll', function(e) {
+//   var scrollTop = window.scrollY;
+//   var offset = getBreakpoint() === 'large' ? 364 : 302;
+//   var isSticky = scrollTop >= offset;
+//   var action = isSticky ? 'add' : 'remove';
+
+//   document.body.classList[action]('is-headerSticky');
+// });
+
+console.log('hey');
+window.onload = function() {
+  let myiFrame = document.getElementById("signup");
+  console.log('hi', myiFrame);
+  let doc = myiFrame.contentDocument;
+  doc.body.innerHTML = doc.body.innerHTML + `
+    <style>
+      .embed-page {
+        padding: 0;
+      }
+    </style>
+  `;
 }
-
-window.addEventListener('scroll', function(e) {
-  var scrollTop = window.scrollY;
-  var offset = getBreakpoint() === 'large' ? 364 : 302;
-  var isSticky = scrollTop >= offset;
-  var action = isSticky ? 'add' : 'remove';
-
-  document.body.classList[action]('is-headerSticky');
-});
 
